838db2c8f871c78624a05be2c08c04377d15757f
Rafał Łasocha <me@swistak35.com>
2017-10-15 23:02:10 +0200

    Finished assgn1/task4

--- assignment1/tasks.rkt
+++ assignment1/tasks.rkt
@@ -2,6 +2,8 @@
 
 ; I wasn't using eopl because it wasn't displaying the results by default and atom? was undefined there anyway
 
+(require racket/trace)
+
 ; Task 1 (1.21)
 (define product
   (lambda (sos1 sos2) (product2 sos2 sos1 sos2 '())))
@@ -33,15 +35,48 @@
 
 ; Task 3 (1.28)
 
-(define (merge loi1 loi2)
+(define (merge loi1 loi2) (merge/predicate < loi1 loi2))
+(define (merge/predicate pred loi1 loi2)
   (cond
     ((null? loi1) loi2)
     ((null? loi2) loi1)
-    ((< (car loi1) (car loi2)) (cons (car loi1) (merge (cdr loi1) loi2)))
-    (else (cons (car loi2) (merge loi1 (cdr loi2))))))
+    ((pred (car loi1) (car loi2)) (cons (car loi1) (merge/predicate pred (cdr loi1) loi2)))
+    (else (cons (car loi2) (merge/predicate pred loi1 (cdr loi2))))))
 
 (merge '(1 4) '(1 2 8))
 (merge '(35 62 81 90 91) '(3 83 85 90))
 
+; Task 4 (1.30)
 
+(define (one? lst)
+  (cond
+    ((null? (cdr lst)) #t)
+    (else #f)))
+
+(one? '(a))
+(one? '(a b))
+
+(define (split lst) (split/acc lst (cons '() '())))
+(define (split/acc lst acc)
+  (cond
+    ((null? lst) acc)
+    ((one? lst) (cons (cons (car lst) (car acc)) (cdr acc)))
+    (else (split/acc (cddr lst) (cons (cons (car lst) (car acc)) (cons (cadr lst) (cdr acc)))))))
+
+(split '())
+(split '(a))
+(split '(a b))
+(split '(a b c d e f))
+
+(define (sort/predicate pred loi)
+  (cond
+    ((null? loi) loi)
+    ((one? loi) loi)
+    (else (let* ([loi-splitted (split loi)]
+                 [loi-first (car loi-splitted)]
+                 [loi-second (cdr loi-splitted)])
+            (merge/predicate pred (sort/predicate pred loi-first) (sort/predicate pred loi-second))))))
+
+(sort/predicate < '(8 2 5 2 3))
+(sort/predicate > '(8 2 5 2 3))
 
