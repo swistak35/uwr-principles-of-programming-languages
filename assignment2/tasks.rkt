068adacaaaad6a2e6128fa375a74e8caf5489af7
Rafał Łasocha <me@swistak35.com>
2017-10-22 21:32:35 +0200

    assgn2/task2

--- assignment2/tasks.rkt
+++ assignment2/tasks.rkt
@@ -33,3 +33,17 @@
 (check-equal? (apply-env (extend-env 'x 42 (empty-env)) 'x) 42)
 (check-equal? (apply-env (extend-env 'x 11 (extend-env 'x 42 (empty-env))) 'x) 11)
 (check-exn #rx"No binding for x" (lambda () (apply-env (empty-env) 'x)))
+
+; Task 2 (2.10)
+
+(define (report-argument-error)
+  (eopl:error 'extend-var* "Argument error"))
+
+(define (extend-env* vars vals saved-env)
+  (cond
+    ((and (null? vars) (null? vals)) saved-env)
+    ((or (null? vars) (null? vals)) (report-argument-error))
+    (else (extend-env* (cdr vars) (cdr vals) (extend-env (car vars) (car vals) saved-env)))))
+
+(check-equal? (extend-env* '(x y) '(42 11) (empty-env)) '((y . 11) (x . 42)))
+(check-equal? (extend-env* '(x y x) '(42 11 4) (empty-env)) '((x . 4) (y . 11) (x . 42)))
