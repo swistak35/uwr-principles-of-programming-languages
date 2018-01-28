0df87e02e7637fe330169b448becd1a57bcec044
Rafał Łasocha <me@swistak35.com>
2018-01-28 19:00:37 +0100

    Make repl more advanced

--- project/run-repl.rkt
+++ project/run-repl.rkt
@@ -1,16 +1,53 @@
 #lang eopl
 
 (require (only-in racket/base
-                  read-line displayln eof-object? printf
+                  read-line displayln eof-object? printf format
                   exit))
+(require (only-in racket/string
+                  string-join))
+(require "lang.rkt")
+(require "data-structures.rkt")
+(require "prettyprint-exp.rkt")
+(require (only-in "interp.rkt"
+                  value-of-program))
+(require (only-in "inference.rkt"
+                  infer/pgm))
+(require (only-in "prettyprint-type.rkt"
+                  prettyprint-type))
 
 (define prompt "> ")
 
 (define (handle-exit)
   (exit))
 
-(define (handle-unknown cmd)
-  (printf "Unknown input: ~s\n" cmd))
+(define (prettyprint-expval val)
+  (cases expval val
+    (num-val (num) num)
+    (bool-val (bool) bool)
+    (list-val (lst)
+      (format
+        "[~a]"
+        (string-join (map prettyprint-expval lst) ", ")))
+    (ref-val (ref)
+      (format "&~a" ref))
+    (proc-val (p)
+      (cases proc p
+        (procedure (bvars body env)
+          (format
+            "procedure<(~a); ~a>"
+            (string-join (map symbol->string bvars) ", ")
+            (prettyprint-exp body)))
+        (primitive (name)
+          (format "primitive<~a>" name))))))
+
+(define (handle-interpret str)
+  (let* ((prg (scan&parse str))
+         (result-type (infer/pgm prg))
+         (result-value (value-of-program prg)))
+    (printf
+      "~a : ~a\n\n"
+      (prettyprint-expval result-value)
+      (prettyprint-type result-type))))
 
 (define (repl-rec)
   (display prompt)
@@ -18,7 +55,7 @@
     (cond
       ((eof-object? command-read) (handle-exit))
       ((equal? command-read "exit") (handle-exit))
-      (else (handle-unknown command-read))))
+      (else (handle-interpret command-read))))
   (repl-rec))
 
 (define (repl)
